5aae1b8786c197a1d5b7e1bf9bf3d46440c3c788
ManalMahmoud <manalmahmoud1311@outlook.com>
2018-04-25 10:55:30 +0200

    adding needed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//for routing configuration
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
-
+const appRoutes:Routes=[
+  {path:'home' ,component:HomeComponent},
+  {path:'home/:id' ,component:HomeComponent},
+  {path:'product',component:ProductComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
